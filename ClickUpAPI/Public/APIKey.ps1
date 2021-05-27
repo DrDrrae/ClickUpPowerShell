81439d091f198ce6adf33d8f11009f613fba6702
DrDrrae <drdrrae@gmail.com>
2021-05-26 20:54:30 -0400

    APIKey help

--- ClickUpAPI/Public/APIKey.ps1
+++ ClickUpAPI/Public/APIKey.ps1
@@ -1,8 +1,23 @@
-﻿function Add-ClickUpAPIKey {
+﻿<#
+.SYNOPSIS
+    Add a ClickUp API key to the current session.
+.DESCRIPTION
+    Add a ClickUp API key to the current session.
+.PARAMETER APIKey
+    The ClickUP API key to store.
+.EXAMPLE
+    PS C:\> Add-ClickUpAPIKey -APIKey pk_11111111_AAAAAAAAAAAAAAAAAAA1111111111111
+    Add the ClickUp API key "pk_11111111_AAAAAAAAAAAAAAAAAAA1111111111111" to the current session.
+.INPUTS
+    None
+.OUTPUTS
+    None
+#>
+function Add-ClickUpAPIKey {
     [CmdletBinding()]
-
-    Param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [OutputType([System.Void])]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = 'The ClickUP API key to store.')]
         [ValidateNotNullOrEmpty()]
         [Alias('Api_Key')]
         [string]$APIKey
@@ -21,17 +36,44 @@
     }
 }
 
+<#
+.SYNOPSIS
+    Remove the ClickUP API key from the current session.
+.DESCRIPTION
+    Remove the ClickUP API key from the current session.
+.EXAMPLE
+    PS C:\> Remove-ClickUpAPIKey
+    Remove the ClickUP API key from the current session.
+.INPUTS
+    None
+.OUTPUTS
+    None
+#>
 function Remove-ClickUpAPIKey {
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    [OutputType([System.Void])]
     param ()
     if ($PSCmdlet.ShouldProcess($ClickUpAPIKey)) {
         Remove-Variable -Name 'ClickUpAPIKey' -Scope Global -Force
     }
 }
 
+<#
+.SYNOPSIS
+    Return the secure ClickUp API Key from the current session.
+.DESCRIPTION
+    Return the secure ClickUp API Key from the current session.
+.EXAMPLE
+    PS C:\> Get-ClickUpAPIKey
+    Return the secure ClickUp API Key from the current session.
+.INPUTS
+    None
+.OUTPUTS
+    System.Security.SecureString
+#>
 function Get-ClickUpAPIKey {
     [CmdletBinding()]
-    [OutputType([System.Management.Automation.PSCustomObject])]
+    [OutputType([System.Security.SecureString])]
     param ()
     if ($null -eq $ClickUpAPIKey) {
         Write-Error 'No API key exists. Please run Add-ClickUpAPIKey to add one.'
