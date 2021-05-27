9ababfa950d6a9f4779a067ac03ddaed16109837
DrDrrae <drdrrae@gmail.com>
2021-05-26 20:54:43 -0400

    Attachment help and custom task IDs

--- ClickUpAPI/Public/Attachments.ps1
+++ ClickUpAPI/Public/Attachments.ps1
@@ -3,8 +3,20 @@
     Upload an attachment to a ClickUp task.
 .DESCRIPTION
     Upload an attachment to a ClickUp task.
+.PARAMETER TaskID
+    The ClickUp TaskID. Could also be a custom ID with the -CustomTaskIDs and -TeamID parameters provided.
+.PARAMETER AttachmentPath
+    The path to the attachment to upload.
+.PARAMETER CustomTaskIDs
+    Set to $true if the task ID provided is a custom ID.
+.PARAMETER TeamID
+    Required ClickUp team ID if -CustomTaskIDs is set to $true.
 .EXAMPLE
-    PS C:\> New-ClickUpTaskAttachment
+    PS C:\> New-ClickUpTaskAttachment -TaskID 9hv -AttachmentPath "C:\Path\To\File.txt"
+    Upload file "File.txt" as an attachment to ClickUp task with ID "9hv".
+.EXAMPLE
+    PS C:\> New-ClickUpTaskAttachment -TaskID CustomTaskID -AttachmentPath "C:\Path\To\File.txt" -CustomTaskIDs $true -TeamID 123
+    Upload file "File.txt" as an attachment to ClickUp task with custom ID "CustomTaskID".
 .INPUTS
     None
 .OUTPUTS
@@ -15,13 +27,19 @@
     https://jsapi.apiary.io/apis/clickup20/reference/0/attachments/create-task-attachment.html
 #>
 function New-ClickUpTaskAttachment {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'TaskID')]
     [OutputType([System.Management.Automation.PSCustomObject])]
     param(
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'TaskIDs', HelpMessage = 'The ClickUp TaskID.')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'CustomTaskIDs', HelpMessage = 'The ClickUp custom TaskID.')]
         [string]$TaskID,
-        [Parameter(Mandatory = $true)]
-        [string]$AttachmentPath
+        [Parameter(Mandatory = $true, ParameterSetName = 'TaskIDs', HelpMessage = 'The path to the attachment to upload.')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'CustomTaskIDs', HelpMessage = 'The path to the attachment to upload.')]
+        [string]$AttachmentPath,
+        [Parameter(Mandatory = $true, ParameterSetName = 'CustomTaskIDs')]
+        [bool]$CustomTaskIDs,
+        [Parameter(Mandatory = $true, ParameterSetName = 'CustomTaskIDs')]
+        [UInt32]$TeamID
     )
 
     $FileBytes = [System.IO.File]::ReadAllBytes($AttachmentPath);
