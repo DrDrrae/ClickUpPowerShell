e5c28158f280136a227da2efe396e50027f70c6d
DrDrrae <drdrrae@gmail.com>
2021-05-30 13:10:41 -0400

    Update team help-docs

--- ClickUpAPI/Public/Team.ps1
+++ ClickUpAPI/Public/Team.ps1
@@ -1,4 +1,21 @@
-﻿function Get-ClickUpTeam {
+﻿<#
+.SYNOPSIS
+    Get all ClickUp teams.
+.DESCRIPTION
+    Get all ClickUp teams.
+.EXAMPLE
+    PS C:\> Get-ClickUpTeam
+    Get all ClickUp teams the authorized user has access to.
+.INPUTS
+    None
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+.NOTES
+    See the link for information.
+.LINK
+    https://jsapi.apiary.io/apis/clickup20/reference/0/teams/get-teams.html
+#>
+function Get-ClickUpTeam {
     [CmdletBinding()]
     [OutputType([System.Management.Automation.PSCustomObject])]
     param ()
