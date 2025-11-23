d2bb7650c7e9e77ecbe451bcad3413479e1f2f07
Andrea Mastellone <drdrrae@gmail.com>
2025-11-23 17:25:38 -0500

    Add Get-ClickUpCustomTaskTypes function
    Introduces Get-ClickUpCustomTaskTypes to retrieve custom task types for a specified ClickUp team. Updates module manifest to include the new public function.

---
+++ ClickUpAPI/Public/CustomTaskTypes.ps1
@@ -0,0 +1,28 @@
+﻿
+<#
+.SYNOPSIS
+    Get Clickup Custom Task Types.
+.DESCRIPTION
+    Get Clickup Custom Task Types.
+.EXAMPLE
+    PS C:\> Get-ClickUpCustomTaskTypes -TeamID 123
+    Get Clickup Custom Task Types for Team with ID "123".
+.INPUTS
+    None
+.OUTPUTS
+    System.Object Array.
+.NOTES
+    See the link for information.
+.LINK
+    https://developer.clickup.com/reference/getcustomitems
+#>
+function Get-ClickUpCustomTaskTypes {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [uint64]$TeamID
+    )
+
+    $CustomTaskTypes = Invoke-ClickUpAPIGet -Arguments $QueryString -Endpoint "team/$TeamID/custom_item"
+    return $CustomTaskTypes.custom_items
+}
