faa02ef21a881027ae434c54328620894a75b8a8
Andrea Mastellone <drdrrae@gmail.com>
2025-11-24 16:45:45 -0500

    Improve Get-ClickUpTaskTemplates logging and error handling
    Enhanced the Get-ClickUpTaskTemplates function with verbose logging for better traceability and added try/catch error handling to provide clearer error messages when API calls fail. Also corrected example descriptions for accuracy.

--- ClickUpAPI/Public/TaskTemplates.ps1
+++ ClickUpAPI/Public/TaskTemplates.ps1
@@ -5,10 +5,10 @@
     Get all ClickUp task templates.
 .EXAMPLE
     PS C:\> Get-ClickUpTaskTemplates -TeamID 512
-    Get all ClickUp team goals for team with ID "123".
+    Get all ClickUp task templates for team with ID "512".
 .EXAMPLE
     PS C:\> Get-ClickUpTaskTemplates -TeamID 512 -Page 1
-    Get all ClickUp team goals for team with ID "123" starting on page 2.
+    Get all ClickUp task templates for team with ID "512" starting on page 2.
 .INPUTS
     None. This cmdlet does not accept any input.
 .OUTPUTS
@@ -30,10 +30,18 @@ function Get-ClickUpTaskTemplates {
         [UInt64]$Page = 0
     )
 
+    Write-Verbose 'Entering Get-ClickUpTaskTemplates'
     $QueryString = @{
         page = $Page
     }
 
-    $Templates = Invoke-ClickUpAPIGet -Arguments $QueryString -Endpoint "team/$TeamID/taskTemplate"
-    return $Templates.templates
+    try {
+        Write-Verbose "Getting task templates for team ID: $TeamID (Page: $Page)"
+        $Templates = Invoke-ClickUpAPIGet -Arguments $QueryString -Endpoint "team/$TeamID/taskTemplate"
+        Write-Verbose 'Successfully retrieved task templates'
+        return $Templates.templates
+    } catch {
+        Write-Error "Error in Get-ClickUpTaskTemplates: $($_.Exception.Message)"
+        throw $_
+    }
 }
