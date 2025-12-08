0d0e86eeca1c3288b363f49921fc67db2a801a93
Andrea Mastellone <drdrrae@gmail.com>
2025-12-08 14:30:21 -0500

    Enable TeamID pipeline input for Get-ClickUpSharedHierarchy
    Updated the Get-ClickUpSharedHierarchy cmdlet to accept TeamID via pipeline input by property name, added relevant aliases, and improved documentation with an additional example for pipeline usage. This enhances integration with other cmdlets and streamlines workflow automation.

--- ClickUpAPI/Public/SharedHierarchy.ps1
+++ ClickUpAPI/Public/SharedHierarchy.ps1
@@ -2,20 +2,22 @@
 .SYNOPSIS
     Get ClickUp shared hierarchy.
 .DESCRIPTION
-    Get ClickUp shared hierarchy. Returns all resources you have access to where you don't have access to its parent. For example, if you have a access to a shared task, but don't have access to its parent list, it will come back in this request.
+    Get ClickUp shared hierarchy. Returns all resources you have access to where you don't have access to its parent. For example, if you have access to a shared task, but don't have access to its parent list, it will come back in this request. Can accept TeamID via pipeline input for integration with other cmdlets.
 .EXAMPLE
     PS C:\> Get-ClickUpSharedHierarchy -TeamID 123
     Returns all ClickUp resources you have access to where you don't have access to its parent.
+.EXAMPLE
+    PS C:\> Get-ClickUpAuthorizedWorkspaces | Get-ClickUpTeam | Get-ClickUpSharedHierarchy
+    Gets shared hierarchy by piping team ID from Get-ClickUpTeam.
 .INPUTS
-    None. This cmdlet does not accept any input.
+    System.UInt64. TeamID via pipeline by property name.
 .OUTPUTS
     System.Object
-.OUTPUTS
     System.Array
 .NOTES
     See the link for information.
 
-    Returns all resources you have access to where you don't have access to its parent. For example, if you have a access to a shared task, but don't have access to its parent list, it will come back in this request.
+    Returns all resources you have access to where you don't have access to its parent. For example, if you have access to a shared task, but don't have access to its parent list, it will come back in this request.
 .LINK
     https://developer.clickup.com/reference/sharedhierarchy
 #>
@@ -23,8 +25,9 @@ function Get-ClickUpSharedHierarchy {
     [CmdletBinding()]
     [OutputType([System.Object], [System.Array])]
     param (
-        [Parameter(Mandatory = $true)]
-        [ulong]$TeamID
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias('team_id', 'id')]
+        [uint64]$TeamID
     )
 
     Write-Verbose 'Entering Get-ClickUpSharedHierarchy'
